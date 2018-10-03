abf2d20633831066de17d2f7c4720fdf427a3542
Kévin Hilairet <kevin@octopouce.mu>
2018-10-03 17:00:40 +0400

    update

--- Form/FieldType.php
+++ Form/FieldType.php
@@ -6,25 +6,26 @@
 
 namespace Octopouce\CmsBundle\Form;
 
-use Octopouce\AdminBundle\Form\Type\SwitchType;
-use Octopouce\CmsBundle\Entity\Block;
 use Octopouce\CmsBundle\Entity\Field;
-use Octopouce\CmsBundle\Entity\PageBlock;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FieldType extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
+		$builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+			$field = $event->getData();
+			$form = $event->getForm();
 
-		$builder
-			->add('value', TextareaType::class)
-		;
+			$form->add('value', TextareaType::class, [
+				'label' => $field->getName()
+			]);
+		});
 	}
 
 	public function configureOptions(OptionsResolver $resolver)
